51afe8b45f35a718951540c0c479291bb3fc88b2
Maxence Royer <maxence.royer@worldline.com>
2017-03-24 15:53:46 +0100

    [MRR] Add links on FluxRss titles

--- soap/templates_c/54927e2478ea890e5dfb96425a0f32d2eb54acb3_0.file.footer.tpl.php
+++ soap/templates_c/54927e2478ea890e5dfb96425a0f32d2eb54acb3_0.file.footer.tpl.php
@@ -1,18 +1,18 @@
 <?php
-/* Smarty version 3.1.30, created on 2017-03-24 13:05:08
+/* Smarty version 3.1.30, created on 2017-03-24 14:11:04
   from "C:\wamp64\www\tli-acupuncture\views\components\footer.tpl" */
 
 /* @var Smarty_Internal_Template $_smarty_tpl */
 if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   'version' => '3.1.30',
-  'unifunc' => 'content_58d519848384a5_70236854',
+  'unifunc' => 'content_58d528f87969c9_22141312',
   'has_nocache_code' => false,
   'file_dependency' => 
   array (
     '54927e2478ea890e5dfb96425a0f32d2eb54acb3' => 
     array (
       0 => 'C:\\wamp64\\www\\tli-acupuncture\\views\\components\\footer.tpl',
-      1 => 1487672347,
+      1 => 1490360999,
       2 => 'file',
     ),
   ),
@@ -20,14 +20,14 @@ if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   array (
   ),
 ),false)) {
-function content_58d519848384a5_70236854 (Smarty_Internal_Template $_smarty_tpl) {
+function content_58d528f87969c9_22141312 (Smarty_Internal_Template $_smarty_tpl) {
 ?>
 		<!-- Adjustment -->
         <div class="clear_both"></div>
 
 		<!-- Footer -->
 		<footer class="footer_general" role="contentinfo">
-		  <p>Une réalisation 2017 par Cordier Camille et Royer Maxence (4IRC). Tous droits réservés. &copy;</p>
+		  <p>Une réalisation 2017 par Cordier Camille, Royer Maxence, Urbe Sébastien et Delorme Claire (4IRC). Tous droits réservés. &copy;</p>
 		</footer>
 	</body>
 </html><?php }
--- templates_c/6bdc32ce03a2a10302a001eba2509c10afa954fc_0.file.home.tpl.php
+++ templates_c/6bdc32ce03a2a10302a001eba2509c10afa954fc_0.file.home.tpl.php
@@ -1,18 +1,18 @@
 <?php
-/* Smarty version 3.1.30, created on 2017-03-24 13:03:08
+/* Smarty version 3.1.30, created on 2017-03-24 14:50:57
   from "C:\wamp64\www\tli-acupuncture\views\content-pages\home.tpl" */
 
 /* @var Smarty_Internal_Template $_smarty_tpl */
 if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   'version' => '3.1.30',
-  'unifunc' => 'content_58d5190c299539_56509877',
+  'unifunc' => 'content_58d53251973904_22971354',
   'has_nocache_code' => false,
   'file_dependency' => 
   array (
     '6bdc32ce03a2a10302a001eba2509c10afa954fc' => 
     array (
       0 => 'C:\\wamp64\\www\\tli-acupuncture\\views\\content-pages\\home.tpl',
-      1 => 1490020223,
+      1 => 1490367053,
       2 => 'file',
     ),
   ),
@@ -20,7 +20,7 @@ if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   array (
   ),
 ),false)) {
-function content_58d5190c299539_56509877 (Smarty_Internal_Template $_smarty_tpl) {
+function content_58d53251973904_22971354 (Smarty_Internal_Template $_smarty_tpl) {
 ?>
 		<!-- Content -->
 		<div id="content_page" role="main">
@@ -33,8 +33,9 @@ foreach ($_from as $_smarty_tpl->tpl_vars['arrayItem']->value) {
 ?>
 					<section class="rss_item">
 						<img src="views/statics/img/RSS.png" class="rss_icon" />
-						<b><?php echo $_smarty_tpl->tpl_vars['arrayItem']->value->getTitle();?>
-</b>
+						<a href="<?php echo $_smarty_tpl->tpl_vars['arrayItem']->value->getLink();?>
+" target="_blank"><?php echo $_smarty_tpl->tpl_vars['arrayItem']->value->getTitle();?>
+</a>
 						<hr />
 						<?php echo $_smarty_tpl->tpl_vars['arrayItem']->value->getDescription();?>
 
