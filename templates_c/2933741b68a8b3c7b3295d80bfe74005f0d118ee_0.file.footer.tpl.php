db5b53b0ce2cf1ef43691eddb5c5032beb7bb482
Maxence Royer <maxence.royer@worldline.com>
2017-03-24 14:20:32 +0100

    [MRR] Correction of SymptomesDetails template

--- templates_c/2933741b68a8b3c7b3295d80bfe74005f0d118ee_0.file.footer.tpl.php
+++ templates_c/2933741b68a8b3c7b3295d80bfe74005f0d118ee_0.file.footer.tpl.php
@@ -1,18 +1,18 @@
 <?php
-/* Smarty version 3.1.30, created on 2017-02-21 10:19:23
+/* Smarty version 3.1.30, created on 2017-03-24 13:11:17
   from "C:\wamp64\www\tli-acupuncture\views\components\footer.tpl" */
 
 /* @var Smarty_Internal_Template $_smarty_tpl */
 if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   'version' => '3.1.30',
-  'unifunc' => 'content_58ac142bce5536_75127213',
+  'unifunc' => 'content_58d51af5c8ee70_11680064',
   'has_nocache_code' => false,
   'file_dependency' => 
   array (
     '2933741b68a8b3c7b3295d80bfe74005f0d118ee' => 
     array (
       0 => 'C:\\wamp64\\www\\tli-acupuncture\\views\\components\\footer.tpl',
-      1 => 1487672347,
+      1 => 1490360999,
       2 => 'file',
     ),
   ),
@@ -20,14 +20,14 @@ if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   array (
   ),
 ),false)) {
-function content_58ac142bce5536_75127213 (Smarty_Internal_Template $_smarty_tpl) {
+function content_58d51af5c8ee70_11680064 (Smarty_Internal_Template $_smarty_tpl) {
 ?>
 		<!-- Adjustment -->
         <div class="clear_both"></div>
 
 		<!-- Footer -->
 		<footer class="footer_general" role="contentinfo">
-		  <p>Une réalisation 2017 par Cordier Camille et Royer Maxence (4IRC). Tous droits réservés. &copy;</p>
+		  <p>Une réalisation 2017 par Cordier Camille, Royer Maxence, Urbe Sébastien et Delorme Claire (4IRC). Tous droits réservés. &copy;</p>
 		</footer>
 	</body>
 </html><?php }
--- templates_c/54927e2478ea890e5dfb96425a0f32d2eb54acb3_0.file.footer.tpl.php
+++ templates_c/54927e2478ea890e5dfb96425a0f32d2eb54acb3_0.file.footer.tpl.php
@@ -1,18 +1,18 @@
 <?php
-/* Smarty version 3.1.30, created on 2017-03-24 13:03:08
+/* Smarty version 3.1.30, created on 2017-03-24 13:11:17
   from "C:\wamp64\www\tli-acupuncture\views\components\footer.tpl" */
 
 /* @var Smarty_Internal_Template $_smarty_tpl */
 if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   'version' => '3.1.30',
-  'unifunc' => 'content_58d5190c5d9684_04138663',
+  'unifunc' => 'content_58d51af5a87576_61386113',
   'has_nocache_code' => false,
   'file_dependency' => 
   array (
     '54927e2478ea890e5dfb96425a0f32d2eb54acb3' => 
     array (
       0 => 'C:\\wamp64\\www\\tli-acupuncture\\views\\components\\footer.tpl',
-      1 => 1487672347,
+      1 => 1490360999,
       2 => 'file',
     ),
   ),
@@ -20,14 +20,14 @@ if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   array (
   ),
 ),false)) {
-function content_58d5190c5d9684_04138663 (Smarty_Internal_Template $_smarty_tpl) {
+function content_58d51af5a87576_61386113 (Smarty_Internal_Template $_smarty_tpl) {
 ?>
 		<!-- Adjustment -->
         <div class="clear_both"></div>
 
 		<!-- Footer -->
 		<footer class="footer_general" role="contentinfo">
-		  <p>Une réalisation 2017 par Cordier Camille et Royer Maxence (4IRC). Tous droits réservés. &copy;</p>
+		  <p>Une réalisation 2017 par Cordier Camille, Royer Maxence, Urbe Sébastien et Delorme Claire (4IRC). Tous droits réservés. &copy;</p>
 		</footer>
 	</body>
 </html><?php }
