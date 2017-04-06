7e2ce58aa3b43c6d69223d8c7e192591a575d01b
Maxence Royer <maxence.royer@worldline.com>
2017-04-06 21:00:58 +0200

    [MRR] WS correction of URLs

--- soap/templates_c/7e3d79cba75c79b0fddf134797959c701347c5bd_0.file.ws.tpl.php
+++ soap/templates_c/7e3d79cba75c79b0fddf134797959c701347c5bd_0.file.ws.tpl.php
@@ -1,18 +1,18 @@
 <?php
-/* Smarty version 3.1.30, created on 2017-04-05 11:52:23
+/* Smarty version 3.1.30, created on 2017-04-06 18:59:53
   from "C:\wamp64\www\tli-acupuncture\views\content-pages\ws.tpl" */
 
 /* @var Smarty_Internal_Template $_smarty_tpl */
 if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   'version' => '3.1.30',
-  'unifunc' => 'content_58e4da7771e5e0_54051847',
+  'unifunc' => 'content_58e69029ea8ba3_43632564',
   'has_nocache_code' => false,
   'file_dependency' => 
   array (
     '7e3d79cba75c79b0fddf134797959c701347c5bd' => 
     array (
       0 => 'C:\\wamp64\\www\\tli-acupuncture\\views\\content-pages\\ws.tpl',
-      1 => 1491327369,
+      1 => 1491505192,
       2 => 'file',
     ),
   ),
@@ -20,7 +20,7 @@ if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   array (
   ),
 ),false)) {
-function content_58e4da7771e5e0_54051847 (Smarty_Internal_Template $_smarty_tpl) {
+function content_58e69029ea8ba3_43632564 (Smarty_Internal_Template $_smarty_tpl) {
 ?>
 		  <!-- Content -->
           <div id="content_page" role="main" style="float:none;width:100%;">
@@ -37,28 +37,28 @@ function content_58e4da7771e5e0_54051847 (Smarty_Internal_Template $_smarty_tpl)
 			  <section class="stripped">
 				  <h3>Soustraction</h3>
 				  <p>
-					  <b>URL</b> : http://localhost/tli-acupuncture/soap/calculatrice/soustraction/<b>[NUMBER_1]</b>/<b>[NUMBER_2]</b>
+					  <b>URL</b> : .../tli-acupuncture/soap/calculatrice/soustraction/<b>[NUMBER_1]</b>/<b>[NUMBER_2]</b>
 				  </p>
 				  <p>
-					  <b>Exemple</b> : <a href="http://localhost/tli-acupuncture/soap/calculatrice/soustraction/10/3" title="Exemple de soustraction" target="_blank" tabindex="33">http://localhost/tli-acupuncture/soap/calculatrice/soustraction/10/3</a>
+					  <b>Exemple</b> : <a href="calculatrice/soustraction/10/3" title="Exemple de soustraction" target="_blank" tabindex="33">calculatrice/soustraction/10/3</a>
 				  </p>
 			  </section>
 			  <section class="stripped">
 				  <h3>Multiplication</h3>
 				  <p>
-					  <b>URL</b> : http://localhost/tli-acupuncture/soap/calculatrice/multiplication/<b>[NUMBER_1]</b>/<b>[NUMBER_2]</b>
+					  <b>URL</b> : .../tli-acupuncture/soap/calculatrice/multiplication/<b>[NUMBER_1]</b>/<b>[NUMBER_2]</b>
 				  </p>
 				  <p>
-					  <b>Exemple</b> : <a href="http://localhost/tli-acupuncture/soap/calculatrice/multiplication/10/3" title="Exemple de multiplication" target="_blank" tabindex="34">http://localhost/tli-acupuncture/soap/calculatrice/multiplication/10/3</a>
+					  <b>Exemple</b> : <a href="calculatrice/multiplication/10/3" title="Exemple de multiplication" target="_blank" tabindex="34">calculatrice/multiplication/10/3</a>
 				  </p>
 			  </section>
 			  <section class="stripped">
 				  <h3>Division</h3>
 				  <p>
-					  <b>URL</b> : http://localhost/tli-acupuncture/soap/calculatrice/division/<b>[NUMBER_1]</b>/<b>[NUMBER_2]</b>
+					  <b>URL</b> : ...calculatrice/division/<b>[NUMBER_1]</b>/<b>[NUMBER_2]</b>
 				  </p>
 				  <p>
-					  <b>Exemple</b> : <a href="http://localhost/tli-acupuncture/soap/calculatrice/division/10/3" title="Exemple de division" target="_blank" tabindex="35">http://localhost/tli-acupuncture/soap/calculatrice/division/10/3</a>
+					  <b>Exemple</b> : <a href="calculatrice/division/10/3" title="Exemple de division" target="_blank" tabindex="35">calculatrice/division/10/3</a>
 				  </p>
 			  </section>
           </div><?php }
--- views/content-pages/ws.tpl
+++ views/content-pages/ws.tpl
@@ -13,28 +13,28 @@
 			  <section class="stripped">
 				  <h3>Soustraction</h3>
 				  <p>
-					  <b>URL</b> : http://localhost/tli-acupuncture/soap/calculatrice/soustraction/<b>[NUMBER_1]</b>/<b>[NUMBER_2]</b>
+					  <b>URL</b> : .../tli-acupuncture/soap/calculatrice/soustraction/<b>[NUMBER_1]</b>/<b>[NUMBER_2]</b>
 				  </p>
 				  <p>
-					  <b>Exemple</b> : <a href="http://localhost/tli-acupuncture/soap/calculatrice/soustraction/10/3" title="Exemple de soustraction" target="_blank" tabindex="33">http://localhost/tli-acupuncture/soap/calculatrice/soustraction/10/3</a>
+					  <b>Exemple</b> : <a href="calculatrice/soustraction/10/3" title="Exemple de soustraction" target="_blank" tabindex="33">calculatrice/soustraction/10/3</a>
 				  </p>
 			  </section>
 			  <section class="stripped">
 				  <h3>Multiplication</h3>
 				  <p>
-					  <b>URL</b> : http://localhost/tli-acupuncture/soap/calculatrice/multiplication/<b>[NUMBER_1]</b>/<b>[NUMBER_2]</b>
+					  <b>URL</b> : .../tli-acupuncture/soap/calculatrice/multiplication/<b>[NUMBER_1]</b>/<b>[NUMBER_2]</b>
 				  </p>
 				  <p>
-					  <b>Exemple</b> : <a href="http://localhost/tli-acupuncture/soap/calculatrice/multiplication/10/3" title="Exemple de multiplication" target="_blank" tabindex="34">http://localhost/tli-acupuncture/soap/calculatrice/multiplication/10/3</a>
+					  <b>Exemple</b> : <a href="calculatrice/multiplication/10/3" title="Exemple de multiplication" target="_blank" tabindex="34">calculatrice/multiplication/10/3</a>
 				  </p>
 			  </section>
 			  <section class="stripped">
 				  <h3>Division</h3>
 				  <p>
-					  <b>URL</b> : http://localhost/tli-acupuncture/soap/calculatrice/division/<b>[NUMBER_1]</b>/<b>[NUMBER_2]</b>
+					  <b>URL</b> : ...calculatrice/division/<b>[NUMBER_1]</b>/<b>[NUMBER_2]</b>
 				  </p>
 				  <p>
-					  <b>Exemple</b> : <a href="http://localhost/tli-acupuncture/soap/calculatrice/division/10/3" title="Exemple de division" target="_blank" tabindex="35">http://localhost/tli-acupuncture/soap/calculatrice/division/10/3</a>
+					  <b>Exemple</b> : <a href="calculatrice/division/10/3" title="Exemple de division" target="_blank" tabindex="35">calculatrice/division/10/3</a>
 				  </p>
 			  </section>
           </div>
